567cf28f9c102f46c5329af72b088c1b5e6dbb31
Agatha Turyahikayo <agathaturyahikayo14@gmail.com>
2018-04-07 16:20:56 -0400

    lets hope i can make http requests lol

--- content.js
+++ content.js
@@ -2,25 +2,16 @@
 
 //making http request
 //var API_KEY="";
-var endpointURL=":https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/59ac9c7e-96e4-4288-bdf0-512dc97a8315/url?iterationId=ff3956ac-f5db-4100-98d4-86e7b39107b1";
-var http = function (method, url, body, cb) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url, true);
+var endpointURL="https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/59ac9c7e-96e4-4288-bdf0-512dc97a8315/url?iterationId=ff3956ac-f5db-4100-98d4-86e7b39107b1";
 
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.setRequestHeader('Prediction-Key','711b162034e7406b8f28694424e397ab');//hopefully this line works lol
+var xhttp = new XMLHttpRequest();
+  xttp.setRequestHeader('Content-Type', 'application/json');
+  xttp.setRequestHeader('Prediction-Key','711b162034e7406b8f28694424e397ab');//hopefully this line works lol
+  xhttp.open("POST", "endpointURL", true);
+  xhttp.send();
+    var response = JSON.parse(xhttp.responseText);
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== 4) { return; }
-    if (xhr.status >= 400) {
-      notify('API request failed');
-      console.log('XHR failed', xhr.responseText);
-      return;
-    }
-    cb(JSON.parse(xhr.responseText));
-  };
-  xhr.send(body);
-};
+
 
 // Fetch the API key from config.json on extension startup.
 http('GET', chrome.runtime.getURL('config.json'), '', function (obj) {
